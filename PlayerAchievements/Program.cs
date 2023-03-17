2e1951d8f392270bb6e7a0dfbbf64af205bb8499
Bruno Fernandes <a22103346@alunos.ulht.pt>
2023-03-17 13:26:58 +0000

    Ask for wich achievements the player has

--- PlayerAchievements/Program.cs
+++ PlayerAchievements/Program.cs
@@ -6,11 +6,17 @@ namespace PlayerAchievements
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Qantos jogadores são: ");
+            Console.WriteLine("Quantos jogadores são: ");
             int n = Convert.ToInt32(Console.ReadLine());
 
             Achievements[] myAchievements = new Achievements[n];
-            
+
+            for ( int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Quais são os achievements do Jogador {i}");
+                
+            }
+
         }
     }
 }
